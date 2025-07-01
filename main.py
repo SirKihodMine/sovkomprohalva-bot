a7cf69afa74217f556ac305ccc1c2c48894cd768
Sir Kihod <sirkihod@gmail.com>
2025-07-01 23:39:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-from bot import main  # Из файла bot.py импортируем асинхронную main()
 import asyncio
+from bot import start
+from telegram.ext import ApplicationBuilder
 
-# Запускаем основную функцию
-asyncio.run(main())
+async def main():
+    # Инициализируем бота
+    app = ApplicationBuilder().token("7964769811:AAG6Cvz9VgSms3H0KBZ2MwOTDWFiH1XkwDI").build()
+
+    # Добавляем команды
+    app.add_handler(CommandHandler("start", start))
+
+    print("✅ Бот запущен")
+    await app.run_polling()
+
+if __name__ == '__main__':
+    asyncio.run(main())
