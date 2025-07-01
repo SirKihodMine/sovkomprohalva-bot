9d6e2fd984fcb3868bb9b8db63d5b95c1d96be1d
Sir Kihod <sirkihod@gmail.com>
2025-07-02 00:16:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import asyncio
 import nest_asyncio
 from bot import main
 
-# === Чтобы избежать ошибки с event loop ===
+# Чтобы работать с вложенными event loops
 nest_asyncio.apply()
 
 asyncio.run(main())
